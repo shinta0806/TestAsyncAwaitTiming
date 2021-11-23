6ddf4d6767d9daa956447ed79b3e83cd0149134b
shinta0806 <shinta.0806@gmail.com>
2021-11-23 14:20:52 +0900

    関数内で await

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,56 @@ namespace TestAsyncAwaitTiming
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private const Int32 DELAY_TIME = 1000;
+		private const String URL = "https://www.instagram.com/";
+
+		private HttpClient _httpClient = new();
+
+		private void Write(String message, [CallerMemberName] String memberName = "")
+		{
+			Debug.WriteLine(DateTime.Now.ToString("ss.fff") + " / " + memberName + "() / " + message);
+		}
+
+		private void WriteHead(String message, [CallerMemberName] String memberName = "")
+		{
+			Write(message[..50].Replace("\n", String.Empty), memberName);
+		}
+
+		private async Task<String> SomeTaskAwaitAsync()
+		{
+			Write("Begin");
+
+			// ここで時間がかかる
+			String content = await _httpClient.GetStringAsync(URL);
+
+			WriteHead(content);
+
+			return content;
+		}
+
+		private async void ButtonSimple_Click(object sender, RoutedEventArgs e)
+		{
+			Write("Begin");
+
+			// ここで時間がかかる
+			String content = await _httpClient.GetStringAsync(URL);
+
+			WriteHead(content);
+		}
+
+		private async void ButtonAwait_Click(object sender, RoutedEventArgs e)
+		{
+			Write("Begin");
+
+			// SomeTaskAwaitAsync() の中で await しているが、ここでは時間はかからない
+			Task<String> task = SomeTaskAwaitAsync();
+			Write("After call");
+
+			// ここで時間がかかる
+			String content = await task;
+			WriteHead(content);
 		}
 	}
 }
